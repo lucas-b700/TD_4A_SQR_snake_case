89bae5a4a1802766f0907cb8671d24cceee9fe8d
lucas-b700 <65290583+lucas-b700@users.noreply.github.com>
2023-01-13 10:37:41 +0100

    Début de la partie 2.1.1
    Création des classes Person et Transaction et début des questions de la partie 2.1.1

--- main.py
+++ main.py
@@ -1,1 +1,54 @@
-print("Hello there !")
+from flask import Flask, request
+import json
+
+app = Flask(__name__)
+
+class Transaction:
+    def __init__(self, P1, P2, t, s):
+        self.P1 = P1
+        self.P2 = P2
+        self.t = t
+        self.s = s
+
+class Person:
+    def __init__(self, solde, transactions):
+        self.solde = solde
+        self.transactions = transactions
+
+transactions = []
+people = []
+
+@app.route('/')
+def get_people():
+    return json.dumps([p.__dict__ for p in people])
+
+@app.route('/transactions')
+def get_transactions():
+    return json.dumps([t.__dict__ for t in transactions])
+
+@app.route('/add_person', methods=["POST"])
+def add_person():
+    if request.method == 'POST':
+        solde = request.form.get('solde')
+        people.append(Person(solde, []))
+        return get_people()
+
+@app.route('/add_transaction', methods=["POST"])
+def add_transaction():
+    if request.method == 'POST':
+        P1_index = int(request.form.get('P1'))
+        P2_index = int(request.form.get('P2'))
+	          if 0 <= P1_index < len(people) and 0 <= P2_index < len(people):
+   	  	        P1 = people[P1_index]
+  	  	        P2 = people[P2_index]
+	          else:
+    		        return "Error : Aucune personne n'a été trouvée avec cet indice."
+        t = int(request.form.get('t'))
+        s = int(request.form.get('s'))
+        P1.solde -= s
+        P2.solde += s
+        transaction = Transaction(P1, P2, t, s)
+        transactions.append(transaction)
+        P1.transactions.append(transaction)
+        P2.transactions.append(transaction)
+        return get_transactions()
