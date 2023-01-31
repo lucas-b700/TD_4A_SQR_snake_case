06a601469764f5a1580ad07105fa51146129a406
Maxou <105202869+FLKprod@users.noreply.github.com>
2023-01-31 21:25:13 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import csv
 app = Flask(__name__)
 
 
-# Definition de la classe Transaction
+# Definition de la classe Transaction 
 class Transaction:
 	def __init__(self, P1, P2, t, s):
 		self.P1 = P1
