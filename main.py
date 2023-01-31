c2f4fab146415ae5c61875ca87c74d308b36943c
lucas-b700 <65290583+lucas-b700@users.noreply.github.com>
2023-01-31 16:02:09 +0100

    fixed error in different functions

--- main.py
+++ main.py
@@ -51,13 +51,13 @@ with open('fichierClient.csv', newline='', encoding="utf-8-sig") as csvfile:
 for i in range(1, 8):
 	time = tab[i][2]
 	sum = tab[i][3]
-	for person in people:
-		if person.name == tab[i][0]:
-			_P1 = person
-			person.debit(sum)
-		elif person.name == tab[i][1]:
-			_P2 = person
-			person.credit(sum)
+	for j in range(len(people)):
+            if(people[j].nom == str(tab[i][0])):
+                _P1=people[j]
+                people[j].debit(float(sum))
+            elif(people[j].nom == str(tab[i][1])):
+                _P2=people[j]
+                people[j].credit(float(sum))
 		transaction = Transaction(_P1, _P2, time, sum)
 		transactions[len(transactions) + 1] = transaction
 
@@ -98,15 +98,15 @@ def getSolde(_person = None):
 	
 # fonction pour ajouter une transaction
 @app.route("/<_person1>/<_person2>/<_t>/<_s>", methods = ['PUT']) 
-def addTransaction(_person = None, _person2 = None, _t = None, _s = None): 
+def addTransaction(_person1 = None, _person2 = None, _t = None, _s = None): 
 	if request.method == 'PUT':
-		for person in people:
-			if(person.name == str(_person1)):
-				_person1 = person
-				person.debit(float(_s))
-			elif(person.name == str(_person2)):
-				_person2 = person
-				person.credit(float(_s))
+		for i in range(len(people)):
+			if(people[i].nom == str(_person1)):
+				_person1=people[i]
+				people[i].debit(float(_s))
+			elif(people[i].nom == str(_person2)):
+				_person2=people[i]
+				people[i].credit(float(_s))
 		transaction = Transaction(_person1, _person2, str(_t), float(_s))
 		transactions[len(transactions)+1] = transaction
 		return str(transactions)
