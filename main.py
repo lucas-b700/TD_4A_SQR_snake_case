3727872182b2c03185ec5534afe46943aeb1c8d9
lucas-b700 <65290583+lucas-b700@users.noreply.github.com>
2023-01-31 22:43:49 +0100

    Try to fixed error

--- main.py
+++ main.py
@@ -50,14 +50,14 @@ with open('fichierClient.csv', newline='', encoding="utf-8-sig") as csvfile:
 
 for i in range(0, 5):
 	time = tab[i][2]
-	sum = float(tab[i][3])
+	sum = tab[i][3]
 	for j in range(len(people)):
 		if(people[j].name == str(tab[i+1][0])):
 			_P1 = people[j]
-			people[j].debit(float(sum))
+			people[j].debit(float(tab[i][3]))
 		if(people[j].name == str(tab[i+1][1])):
 			_P2 = people[j]
-			people[j].credit(float(sum))
+			people[j].credit(float(tab[i][3]))
 	transaction = Transaction(_P1, _P2, time, float(sum))
 	transactions[len(transactions) + 1] = transaction
 	
